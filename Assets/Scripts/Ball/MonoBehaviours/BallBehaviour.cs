9b4856b20f571142e87a7f6486829dbff92c2da2
Alexander Travkin <aleverdes.alex@gmail.com>
2024-09-08 19:04:31 +0300

    Fixed crashing bug

--- Assets/Scripts/Ball/MonoBehaviours/BallBehaviour.cs
+++ Assets/Scripts/Ball/MonoBehaviours/BallBehaviour.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TaigaGames.SineysArkanoid.Ball.Services;
+﻿using TaigaGames.SineysArkanoid.Ball.Services;
 using TaigaGames.SineysArkanoid.Level.MonoBehaviours;
 using UnityEngine;
 using Zenject;
@@ -8,8 +7,6 @@ namespace TaigaGames.SineysArkanoid.Ball.MonoBehaviours
 {
     public class BallBehaviour : MonoBehaviour
     {
-        private const float MinVerticalSpeed = 0.2f;
-        
         [field: SerializeField] public SpriteRenderer SpriteRenderer { get; private set; }
         [field: SerializeField] public CircleCollider2D Collider { get; private set; }
         [field: SerializeField] public Rigidbody2D Rigidbody { get; private set; }
@@ -23,33 +20,16 @@ namespace TaigaGames.SineysArkanoid.Ball.MonoBehaviours
             Rigidbody = GetComponent<Rigidbody2D>();
         }
 
-        private void FixedUpdate()
-        {
-            if (Rigidbody.linearVelocity.magnitude < 1f)
-            {
-                Rigidbody.linearVelocity = Rigidbody.linearVelocity.normalized * 1f;
-            }
-            
-            // if (Mathf.Abs(Rigidbody.linearVelocity.normalized.y) < MinVerticalSpeed)
-            // {
-            //     Rigidbody.linearVelocity = Rigidbody.linearVelocity.magnitude * new Vector2(Rigidbody.linearVelocity.x, MinVerticalSpeed * Mathf.Sign(Rigidbody.linearVelocity.y)).normalized;
-            // }
-        }
-
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.gameObject.TryGetComponent<DeathZoneBehaviour>(out var deathZoneBehaviour))
-            {
+            if (other.gameObject.TryGetComponent<DeathZoneBehaviour>(out var deathZoneBehaviour)) 
                 _ballService.DestroyBall(this);
-            }
         }
 
         private void OnCollisionEnter2D(Collision2D other)
         {
-            if (other.gameObject.TryGetComponent<BlockBehaviour>(out var blockBehaviour))
-            {
+            if (other.gameObject.TryGetComponent<BlockBehaviour>(out var blockBehaviour)) 
                 Destroy(blockBehaviour.gameObject);
-            }
         }
     }
 }
