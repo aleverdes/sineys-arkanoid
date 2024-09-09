9956b850b14f49cbda6af94d49ff556fd6717537
Alexander Travkin <aleverdes.alex@gmail.com>
2024-09-09 17:25:23 +0300

    Added more levels

--- Assets/Scripts/Level/Services/BlockService.cs
+++ Assets/Scripts/Level/Services/BlockService.cs
@@ -12,7 +12,7 @@ namespace TaigaGames.SineysArkanoid.Level.Services
 {
     public class BlockService
     {
-        private const float BoosterChance = 0.14f;
+        private const float BoosterChance = 0.12f;
         private const int MaxBallsCount = 10;
 
         [Inject] private readonly DiContainer _diContainer;
